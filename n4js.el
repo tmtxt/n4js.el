6f8816be33b079d503ab592f865c0f084a371218
Truong <me@truongtx.me>
2015-07-11 21:13:23 +0700

    update comment and license

--- n4js.el
+++ n4js.el
@@ -1,6 +1,38 @@
-;;; dependencies: cypher-mode
+;;; n4js.el --- Neo4j Shell for Emacs
 
-;;; emacs neo4j shell
+;;; Copyright (C) 2015 TruongTx
+
+;;; Author: TruongTx <me@truongtx.me>
+;;; Version: 0.1
+;;; URL: https://github.com/tmtxt/n4js.el
+;;; Package-Requires: ((emacs "24"))
+;;; Keywords: neo4j, shell, comint
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Put this file in your Emacs lisp path (e.g. ~/.emacs.d/site-lisp)
+;; and add the following line to your .emacs:
+
+;; (require 'n4js)
+
+;; Type M-x n4js-repl to run Neo4j Shell.
+;; See also `comint-mode' to check key bindings.
+
 (require 'comint)
 (require 'cypher-mode)
 
@@ -111,3 +143,5 @@
     (n4js-start)))
 
 (provide 'n4js)
+
+;;; n4js.el ends here
